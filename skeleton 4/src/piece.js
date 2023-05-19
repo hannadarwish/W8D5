79114b8d9fb290e283b89c4485b1b9e35db2b6e1
hannadarwish <hannadarwish@gmail.com>
2023-05-19 11:42:01 -0700

    finished piece methods

--- skeleton 4/src/piece.js
+++ skeleton 4/src/piece.js
@@ -2,25 +2,41 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
-Piece.prototype.oppColor = function () {
+Piece.prototype.oppColor = function (color) {
+    if (this.color === "white") {
+        return "black";
+    } else {
+        return "white";
+    }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
-Piece.prototype.flip = function () {
+Piece.prototype.flip = function (color) {
+    if (this.oppColor(color) === "white") {
+        this.color = "white";
+    } else {
+        this.color = "black";
+    }
 };
 
 /**
  * Returns a string representation of the piece
  * based on its color.
  */
-Piece.prototype.toString = function () {
+Piece.prototype.toString = function (color) {
+    if (this.color === "white") {
+        return "W";
+    } else {
+        return "B";
+    }
 };
 
 // DON'T TOUCH THIS CODE
